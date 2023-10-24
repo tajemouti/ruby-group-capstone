f5ba838a89ed9940957ef219aad3792471283a59
Mozamel Jawad <mozameljawad@gmail.com>
2023-10-24 12:37:50 +0430

    Complete the options list

--- main.rb
+++ main.rb
@@ -1,7 +1,13 @@
 def print_options
   puts "Options:"
   puts "1. List all books"
-  puts "2. List all labels"
-  puts "3. Add a book"
-  puts "4. Quit"
+  puts "2. List all music albums"
+  puts "3. List of games"
+  puts "4. List all genres"
+  puts "5. List all labels"
+  puts "6. List all authors"
+  puts "7. Add a book"
+  puts "8. Add a music album"
+  puts "9. Add a game"
+  puts "10. Quit"
 end
