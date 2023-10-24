827a13aeef3fd1ceaa3dfe70598bbf7d1499ad9d
tajemouti <tajemouti@gmail.com>
2023-10-25 00:59:53 +0100

    Update main.rb file

--- main.rb
+++ main.rb
@@ -1,5 +1,41 @@
+require './classes/item'
+require './classes/genre'
+require './classes/music_album'
+
+items = []
+
+def list_genres(items)
+  puts 'List of all genres:'
+  genres = items.select { |item| item.is_a?(MusicAlbum) }.map(&:genre)
+  genres.each { |genre| puts "Genre ID: #{genre.id}, Name: #{genre.name}" }
+end
+
+def list_items(class_name, items)
+  items.select { |item| item.is_a?(class_name) }
+end
+
+def add_item(item, items)
+  items << item
+  puts "#{item.class.name} added successfully."
+end
+
+def add_music_album(items)
+  puts 'Enter Album Title:'
+  title = gets.chomp
+  puts 'Is it on Spotify? (true/false):'
+  on_spotify = gets.chomp == 'true'
+  puts 'Enter Genre Name:'
+  genre_name = gets.chomp
+  genre = Genre.new(items.size + 1, genre_name)
+  new_album = MusicAlbum.new(items.size + 1, title, Time.now, on_spotify, genre)
+
+  add_item(new_album, items)
+end
+
+puts 'Welcome to the Cataloge of my things'
+
 def print_options
-  puts 'Options:'
+  puts "\nOptions:"
   puts '1. List all books'
   puts '2. List all music albums'
   puts '3. List of games'
@@ -12,39 +48,35 @@ def print_options
   puts '10. Quit'
 end
 
-def main
-  loop do
-    puts 'Welcome to the Cataloge of my things'
-    print_options
-    print 'Enter your choice: '
-    choice = gets.chomp.to_i
+loop do
+  print_options
+  print 'Enter your choice: '
+  choice = gets.chomp.to_i
 
-    case choice
-    when 1
-      puts 'List of Books:'
-    when 2
-      puts 'List of music albums:'
-    when 3
-      puts 'List of games:'
-    when 4
-      puts 'List of genres:'
-    when 5
-      puts 'List of Labels:'
-    when 6
-      puts 'List of authors:'
-    when 7
-      puts 'Add a book:'
-    when 8
-      puts 'Add a music album:'
-    when 9
-      puts 'Add a game:'
-    when 10
-      puts 'Goodbye!'
-      break
-    else
-      puts 'Invalid choice. Please try again.'
-    end
+  case choice
+  when 1
+    puts 'List of Books:'
+  when 2
+    puts 'List of all music albums:'
+    list_items(MusicAlbum, items).each { |music_album| puts "Album ID: #{music_album.id}, Title: #{music_album.title}" }
+  when 3
+    puts 'List of games:'
+  when 4
+    list_genres(items)
+  when 5
+    puts 'List of Labels:'
+  when 6
+    puts 'List of authors:'
+  when 7
+    puts 'Add a book:'
+  when 8
+    add_music_album(items)
+  when 9
+    puts 'Add a game:'
+  when 10
+    puts 'Goodbye!'
+    break
+  else
+    puts 'Invalid choice. Please try again.'
   end
 end
-
-main
