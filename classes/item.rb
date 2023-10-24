998326ed7e5021e9c1f257fd45c6c25124127579
Mozamel Jawad <mozameljawad@gmail.com>
2023-10-24 12:24:30 +0430

    Add move_to_archive method

--- classes/item.rb
+++ classes/item.rb
@@ -1,9 +1,9 @@
 require 'date'  # Require the Date class
 
 class Item
-  attr_accessor :id, :genre, :author, :source, :label, :publish_date 
+  attr_accessor :id, :genre, :author, :source, :label, :publish_date
   attr_reader :archived
-  
+
   def initialize(id, genre, author, source, label, publish_date, archived)
     @id = id
     @genre = genre
@@ -21,5 +21,14 @@ class Item
       return true
     end
     return false
+  end
+
+  def move_to_archive
+    if can_be_archived?
+      @archived = true
+    end
+  end
+  def label=(label)
+    @label = label
   end
 end
