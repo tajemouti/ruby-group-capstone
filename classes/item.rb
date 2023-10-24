d7e68c020c208634c9a08eca66d1c20f1cf1530a
tajemouti <tajemouti@gmail.com>
2023-10-25 00:44:13 +0100

    Update Item class

--- classes/item.rb
+++ classes/item.rb
@@ -1,29 +1,34 @@
-require 'date' # Require the Date class
-
 class Item
-  attr_accessor :id, :genre, :author, :label, :publish_date
-  attr_reader :archived
+  attr_accessor :title
+  attr_reader :id, :publish_date, :archived
 
-  def initialize(genre, author, label, publish_date, archived)
-    @id = Random.rand(1..1000)
-    @genre = genre
-    @author = author
-    @label = label
+  def initialize(id, title, publish_date)
+    @id = id
+    @title = title
     @publish_date = publish_date
-    @archived = archived
+    @archived = false
   end
 
   def can_be_archived?
-    return false if @publish_date.nil?
-
-    return true if (Date.today - @publish_date.to_date).to_i > 3650
-
-    false
+    !archived
   end
 
   def move_to_archive
-    return unless can_be_archived?
+    @archived = can_be_archived?
+  end
 
-    @archived = true
+  def add_label(label)
+    @label = label
+    label.add_item(self)
+  end
+
+  def add_genre(genre)
+    @genre = genre
+    genre.add_item(self)
+  end
+
+  def add_author(author)
+    @author = author
+    author.add_item(self)
   end
 end
