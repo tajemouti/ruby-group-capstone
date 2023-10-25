ded8c74b5c702bfa377df94e542ae51f048d5b6d
Qaisar <kayser.abbas@gmail.com>
2023-10-25 14:58:29 +0500

    add label.rb

---
+++ classes/label.rb
@@ -0,0 +1,17 @@
+# label.rb
+
+class Label
+  attr_reader :id, :title, :color
+  attr_accessor :items
+
+  def initialize(id, title, color)
+    @id = id
+    @title = title
+    @color = color
+    @items = []
+  end
+
+  def add_item(item)
+      @items << item
+  end
+end
