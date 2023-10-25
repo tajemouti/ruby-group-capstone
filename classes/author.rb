7b8fb5929f733e9c41e03050be5998b4c150143b
Mozamel Jawad <mozameljawad@gmail.com>
2023-10-25 18:20:02 +0430

    Create an Author class with all its method and properties

---
+++ classes/author.rb
@@ -0,0 +1,15 @@
+class Author
+  attr_reader :id, :first_name, :last_name
+  attr_accessor :items
+
+  def initialize(id, first_name, last_name)
+    @id = id
+    @first_name = first_name
+    @last_name = last_name
+    @items = []
+  end
+
+  def add_item(item)
+    @items << item
+  end
+end
