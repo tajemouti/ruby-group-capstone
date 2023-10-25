b47c141b59c10b9ec215383c469e58e8482ea0aa
tajemouti <tajemouti@gmail.com>
2023-10-26 00:56:08 +0100

    Update add_music_album method

--- main.rb
+++ main.rb
@@ -56,12 +56,20 @@ end
 def add_music_album(items)
   puts 'Enter Album Title:'
   title = gets.chomp
+
   puts 'Is it on Spotify? (true/false):'
   on_spotify = gets.chomp == 'true'
+
+  puts 'Enter Publish Date (YYYY-MM-DD):'
+  publish_date = DateTime.parse(gets.chomp)
+
   puts 'Enter Genre Name:'
   genre_name = gets.chomp
+
   genre = Genre.new(items.size + 1, genre_name)
-  new_album = MusicAlbum.new(items.size + 1, title, Time.now, on_spotify, genre)
+  add_item(genre, items)
+
+  new_album = MusicAlbum.new(items.size + 1, title, publish_date.to_time, on_spotify)
   add_item(new_album, items)
 end
 
