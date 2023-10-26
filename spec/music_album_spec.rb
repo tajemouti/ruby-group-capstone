ae20cfdc366aacbd0d8dcc08841c007114a3cde3
tajemouti <tajemouti@gmail.com>
2023-10-26 05:41:34 +0100

    Write unit tests to/from_json methods

--- spec/music_album_spec.rb
+++ spec/music_album_spec.rb
@@ -21,4 +21,32 @@ describe MusicAlbum do
     music_album = MusicAlbum.new(1, 'Album 3', today, not_on_spotify)
     expect(music_album.can_be_archived?).to be false
   end
+
+  it 'returns a JSON representation of the music album' do
+    album = MusicAlbum.new(1, 'Album Title', Date.today, true)
+    expected_json = {
+      'class_name' => 'MusicAlbum',
+      'id' => 1,
+      'title' => 'Album Title',
+      'publish_date' => Date.today,
+      'on_spotify' => true
+    }
+    expect(album.to_json).to eq(expected_json)
+  end
+
+  it 'creates a MusicAlbum object from JSON data' do
+    data = {
+      'class_name' => 'MusicAlbum',
+      'id' => 1,
+      'title' => 'Album Title',
+      'publish_date' => Date.today,
+      'on_spotify' => true
+    }
+    album = MusicAlbum.from_json(data)
+    expect(album).to be_an_instance_of(MusicAlbum)
+    expect(album.id).to eq(1)
+    expect(album.title).to eq('Album Title')
+    expect(album.publish_date).to eq(Date.today)
+    expect(album.on_spotify).to be true
+  end
 end
