b250585d9324f57202517b71e35b852b3a68098a
tajemouti <tajemouti@gmail.com>
2023-10-26 06:16:23 +0100

    Fix linters' errors

--- main.rb
+++ main.rb
@@ -11,7 +11,7 @@ require './classes/game'
 items = []
 def save_items_to_json(items)
   serialized_items = items.map(&:to_json)
-  write_json_to_file('items.json', serialized_items)
+  write_json_to_file('./data/items.json', serialized_items)
 end
 
 def write_json_to_file(file_name, data)
@@ -21,7 +21,7 @@ def write_json_to_file(file_name, data)
 end
 
 def load_items_from_json
-  return [] if File.empty?('items.json')
+  return [] if File.empty?('./data/items.json')
 
   class_mapping = {
     'Book' => Book,
@@ -31,7 +31,7 @@ def load_items_from_json
     'Game' => Game,
     'Author' => Author
   }
-  json_data = JSON.parse(File.read('items.json'))
+  json_data = JSON.parse(File.read('./data/items.json'))
   json_data.map do |item_data|
     class_name = item_data['class_name']
     class_mapping[class_name].from_json(item_data)
