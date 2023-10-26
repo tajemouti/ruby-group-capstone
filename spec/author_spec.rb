334f8a007b20db7adadd69d1b78c93874caba427
Mozamel Jawad <mozameljawad@gmail.com>
2023-10-26 12:49:45 +0430

    add unit test to set the value for first and last name of author

--- spec/author_spec.rb
+++ spec/author_spec.rb
@@ -10,6 +10,14 @@ describe Author do
   before(:each) do
     @item1 = Item.new(11, 'Title', Date.new(2020,01,01))
   end
+  describe 'When testing the Author class' do
+    it 'Sets the first_name attribute' do
+      expect(author.first_name).to eq('Ahmad')
+    end
+    it 'Sets the last_name attribute' do
+      expect(author.last_name).to eq('Ahmadi')
+    end
 
+  end
 
 end
