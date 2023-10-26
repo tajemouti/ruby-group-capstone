c10cc8b9581ad4ac478a9c8cf1fe8a467838218f
Mozamel Jawad <mozameljawad@gmail.com>
2023-10-26 11:31:47 +0430

    Add to_json and from_json method for game class data

--- classes/game.rb
+++ classes/game.rb
@@ -12,4 +12,22 @@ class Game < Item
   def can_be_archived?
     super && (Date.today - @last_played_at.to_date).to_i > 730
   end
+
+  def to_json(*_args)
+    {
+      'class_name' => 'Game',
+      'title' => title,
+      'publish_date' => publish_date.strftime('%Y-%m-%d'),
+      'multiplayer' => multiplayer,
+      'last_played_at' => last_played_at,
+      'author' => author.to_json
+  }
+  end
+
+  def self.from_json(data)
+    author_data = data['author']
+    author = Author.from_json(author_data)
+    Game.new(data['title'], Date.parse(data['publish_date']), data['multiplayer'], data['last_played_at'], author)
+  end
+
 end
