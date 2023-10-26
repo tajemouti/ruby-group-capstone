22dd7e42a10a15b0b0792ca2f576d8674134dda0
tajemouti <tajemouti@gmail.com>
2023-10-26 05:52:28 +0100

    Write unit tests for to/from_json methods from Genre class

--- spec/genre_spec.rb
+++ spec/genre_spec.rb
@@ -13,4 +13,26 @@ describe Genre do
     genre.add_item(music_album)
     expect(genre.items).to include(music_album)
   end
+
+  it 'returns a JSON representation of the genre' do
+    genre = Genre.new(1, 'Rock')
+    expected_json = {
+      'class_name' => 'Genre',
+      'id' => 1,
+      'name' => 'Rock'
+    }
+    expect(genre.to_json).to eq(expected_json)
+  end
+
+  it 'creates a Genre object from JSON data' do
+    data = {
+      'class_name' => 'Genre',
+      'id' => 1,
+      'name' => 'Rock'
+    }
+    genre = Genre.from_json(data)
+    expect(genre).to be_an_instance_of(Genre)
+    expect(genre.id).to eq(1)
+    expect(genre.name).to eq('Rock')
+  end
 end
